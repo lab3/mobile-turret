483ffd8f801e1d83aa7622920e444fe6a85eee71
Len Bucchino <len@bitfly.org>
2017-03-10 22:20:28 -0600

    ios app

---
+++ ios_mobile_turret/Mobile Turret/Mobile Turret/GameViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GameViewController.swift
+//  Mobile Turret
+//
+//  Created by Len on 3/10/17.
+//  Copyright © 2017 redspace. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
